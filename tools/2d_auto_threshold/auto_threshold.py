0a897841c21d267dc015e90658c125024d3ba622
Leonid Kostrykin <void@evoid.de>
2024-03-11 11:36:42 +0000

    Fix issues

--- tools/2d_auto_threshold/auto_threshold.py
+++ tools/2d_auto_threshold/auto_threshold.py
@@ -1,9 +1,8 @@
 """
-Copyright 2017-2022 Biomedical Computer Vision Group, Heidelberg University.
+Copyright 2017-2024 Biomedical Computer Vision Group, Heidelberg University.
 
 Distributed under the MIT license.
 See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
-
 """
 
 import argparse
@@ -13,7 +12,7 @@ import skimage.io
 import skimage.util
 import tifffile
 
-thOptions = {
+th_methods = {
     'otsu': lambda img_raw, bz: skimage.filters.threshold_otsu(img_raw),
     'li': lambda img_raw, bz: skimage.filters.threshold_li(img_raw),
     'yen': lambda img_raw, bz: skimage.filters.threshold_yen(img_raw),
@@ -25,9 +24,9 @@ thOptions = {
 }
 
 
-def auto_thresholding(in_fn, out_fn, th_method, block_size=5, dark_bg=True):
+def do_thresholding(in_fn, out_fn, th_method, block_size=5, dark_bg=True):
     img = skimage.io.imread(in_fn)
-    th = thOptions[th_method](img, block_size)
+    th = th_methods[th_method](img, block_size)
     if dark_bg:
         res = img > th
     else:
@@ -39,9 +38,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Automatic Image Thresholding')
     parser.add_argument('im_in', help='Path to the input image')
     parser.add_argument('im_out', help='Path to the output image (TIFF)')
-    parser.add_argument('th_method', choices=thOptions.keys(), help='Thresholding method')
+    parser.add_argument('th_method', choices=th_methods.keys(), help='Thresholding method')
     parser.add_argument('block_size', type=int, default=5, help='Odd size of pixel neighborhood for calculating the threshold')
     parser.add_argument('dark_bg', default=True, type=bool, help='True if background is dark')
     args = parser.parse_args()
 
-    auto_thresholding(args.im_in, args.im_out, args.th_method, args.block_size, args.dark_bg)
+    do_thresholding(args.im_in, args.im_out, args.th_method, args.block_size, args.dark_bg)
