67679befcba092761c43fd4346202170cdffc807
Leonid Kostrykin <void@evoid.de>
2025-11-17 14:17:48 +0100

    Add commit exclusion

--- tool_years_report/create_report.bash
+++ tool_years_report/create_report.bash
@@ -1,27 +1,37 @@
 #!/usr/bin/bash
 
-find . -name "*.xml" > all_tool_files.txt
-sed -i '/\/creators\.xml/d' all_tool_files.txt
-sed -i '/\/tests\.xml/d' all_tool_files.txt
-sed -i '/\/macros\.xml/d' all_tool_files.txt
-sed -i '/\/macros\.xml/d' all_tool_files.txt
-sed -i '/\/test-data/d' all_tool_files.txt
+export SCRIPTDIR=$(dirname 0)
+export ROOTDIR=$SCRIPTDIR/..
+export TOOLLISTFILE=$ROOTDIR/all_tool_files.txt
+
+find $ROOTDIR -name "*.xml" > $TOOLLISTFILE
+sed -i '/\/creators\.xml/d' $TOOLLISTFILE
+sed -i '/\/tests\.xml/d' $TOOLLISTFILE
+sed -i '/\/macros\.xml/d' $TOOLLISTFILE
+sed -i '/\/macros\.xml/d' $TOOLLISTFILE
+sed -i '/\/test-data/d' $TOOLLISTFILE
 
 echo "Year | Added Tools | Updated Tools"
 echo "-----|-------------|--------------"
 
-tool_files=$(cat all_tool_files.txt)
+tool_files=$(cat $TOOLLISTFILE)
 
 declare -A added
 declare -A updated
 
 for tool in $tool_files; do
+    export tool_rootdir=$(dirname "$tool")
 
     # Get the first commit (added)
-    add_year=$(git log --diff-filter=A --format="%ad" --date=format:'%Y' -- "$tool" | tail -n 1)
+    add_year=$(git log --diff-filter=A --format="%ad" --date=format:'%Y' -- "$tool_rootdir" | tail -n 1)
 
     # Get the years it was updated
-    update_years=$(git log --diff-filter=M --format="%ad" --date=format:'%Y' -- "$tool" | sort -u)
+    update_years=$( \
+        git log --diff-filter=M --format="%H;%ad" --date=format:'%Y' -- "$tool_rootdir" \
+        | grep -v -f $SCRIPTDIR/excluded_commits.txt \
+        | cut -d';' -f2 \
+        | sort -u \
+    )
 
     # Mark as added
     if [[ ! -z $add_year ]]; then
@@ -44,8 +54,11 @@ all_years=$(printf "%s\n" "${!added[@]}" "${!updated[@]}" | sort -u)
 for year in $all_years; do
     add_list=${added[$year]:-}
     upd_list=${updated[$year]:-}
-    #echo "$year | ${add_list%, } | ${upd_list%, }"
-    add_count=$(echo "$add_list" | tr ', ' '\n' | grep -v '^$' | sort -u | wc -l)
-    upd_count=$(echo "$upd_list" | tr ', ' '\n' | grep -v '^$' | sort -u | wc -l)
-    echo "$year | $add_count | $upd_count"
+    if [[ "$1" == "--sum" ]]; then
+        add_count=$(echo "$add_list" | tr ', ' '\n' | grep -v '^$' | sort -u | wc -l)
+        upd_count=$(echo "$upd_list" | tr ', ' '\n' | grep -v '^$' | sort -u | wc -l)
+        echo "$year | $add_count | $upd_count"
+    else
+        echo "$year | ${add_list%, } | ${upd_list%, }"
+    fi
 done
